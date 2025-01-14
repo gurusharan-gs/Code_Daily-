6c7507c3a819dea663dfe867e3b93bf2e58115f9
Gurusharan <gurusharanulagadde143@gmail.com>
2025-01-14 23:27:32 +0530

    dsa

--- zero_to_hero/src/com/basic/MissingNumber.java
+++ zero_to_hero/src/com/basic/MissingNumber.java
@@ -2,6 +2,10 @@ package com.basic;
 
 public class MissingNumber {
 	
+	  /**
+	   * if array contain non-zeros values example like 1,2,4,5
+	   *  use this formula (n+1) * (n+2) / 2;
+	   */
 	  public static int findMissingNumber(int[] arr) {
 		 int n = arr.length;
 		 
@@ -13,10 +17,28 @@ public class MissingNumber {
 		 }
 		 return expectedSum - actualSum;
 	  }
+	  
+	  /**
+	   * if array contain zeros example like 1,0,2,4,5
+	   * use this formula n * (n+1) / 2;
+	   */
+	  public static int missingNumber(int[] nums) {
+		    int n = nums.length;
+		    int expectedSum = n * (n + 1) / 2;
+		    int actualSum = 0;
+		    for (int num : nums) {
+		        actualSum += num;
+		    }
+		    return expectedSum - actualSum;
+		}
 	
 	public static void main(String[] args) {
-		int[] arr = {1,2,3,5,6,7};
-		System.out.println(findMissingNumber(arr));
+		int[] arr1 = {1,2,3,5,6,7};
+		System.out.println(findMissingNumber(arr1));
+		
+		
+		int[] arr2 = {5,1,0,3,4};
+		System.out.println(missingNumber(arr2));
 	}
 
 }
