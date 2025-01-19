ac482fe350d2964cbfe73844bfb592604e4e56b4
Gurusharan <gurusharanulagadde143@gmail.com>
2025-01-19 21:35:45 +0530

    ds

---
+++ zero_to_hero/src/com/day4/Main.java
@@ -0,0 +1,32 @@
+package com.day4;
+
+public class Main {
+	
+	public static int sqrt(int x) {
+		int st = 0, end = x;
+		int ans = -1;
+		
+		while(st <= end) {
+			int mid = st + (end - st) / 2;
+//			int val = mid * mid;
+			long val = (long) mid * mid;
+			
+			if(val == x) {
+				return mid;
+			}else if(val < x) {
+				ans = mid;
+				st = mid + 1;
+			}else {
+				end = mid - 1;
+			}
+		}
+		return ans;
+	}
+	
+	
+	public static void main(String[] args) {
+		int x = 16;
+		System.out.println(sqrt(x));
+	}
+
+}
